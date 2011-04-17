8233ca99c2c2f6d7d726ed011654d1c9119bcbf8
Snidd <m.kjellberg@gmail.com>
2011-04-17 22:55:11 +0200

    Forgot to Add PickViewModel.cs

---
+++ RotisserieDraft/ViewModels/PickViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RotisserieDraft.ViewModels
+{
+    public enum PickTime
+    {
+        History = -1,
+        Current = 0,
+        Future = 1,
+    }
+    public class PickViewModel
+    {
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+        public int CardId { get; set; }
+        public string CardName { get; set; }
+        public PickTime PickTime { get; set; }
+    }
+}
